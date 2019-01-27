c70adc784e849b0a744bc8a7f3157622853f1742
Кутякин Михаил <michakoutjakin@gmail.com>
2019-01-27 18:48:04 +0300

    change config vars

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
 import React, {Component} from 'react';
 import './App.css';
 
-const {REACT_APP_UNSPLASH_ACCESS_KEY, REACT_APP_UNSPLASH_SECRET, REACT_APP_CALLBACK_URL} = process.env;
+const {UNSPLASH_ACCESS_KEY, UNSPLASH_SECRET, CALLBACK_URL} = process.env;
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <header className="App-header">
-                    {REACT_APP_UNSPLASH_ACCESS_KEY} +
-                    {REACT_APP_UNSPLASH_SECRET} +
-                    {REACT_APP_CALLBACK_URL}
+                    {UNSPLASH_ACCESS_KEY} +
+                    {UNSPLASH_SECRET} +
+                    {CALLBACK_URL}
                 </header>
             </div>
         );
